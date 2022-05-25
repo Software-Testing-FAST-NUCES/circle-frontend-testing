8ed5d0a99ec1b6ef00491fe8501f6c382511c6db
unknown <ali.naveed7305@gmail.com>
2022-05-25 19:14:50 +0500

    resetpass

---
+++ POM/resetPass.js
@@ -0,0 +1,17 @@
+module.exports = {
+  url: "http://localhost:3000/",
+  // after: function(browser) {
+  //   browser.end();
+  // },
+  elements: {
+    forgetPassButton: {
+      selector: "ins",
+    },
+    inpField: {
+      selector: "input[id=formBasicEmail]",
+    },
+    nextButton: {
+      selector: "button[class=connect-login-btn]",
+    },
+  },
+};
